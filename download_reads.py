01cd805e2c85bd4878196d5aa2ed1239f9e962d0
scwatts <s.watts2@student.unimelb.edu.au>
2018-02-01 14:54:37 +1100

    download_reads: changed name scheme

--- download_reads.py
+++ download_reads.py
@@ -467,14 +467,13 @@ class SraRun(object):
         if self.experiment.platform == 'ILLUMINA':
             old_name_1 = self.accession + '_1.fastq.gz'
             old_name_2 = self.accession + '_2.fastq.gz'
-            new_name_1 = self.get_filename_base() + '_1.fastq.gz'
-            new_name_2 = self.get_filename_base() + '_2.fastq.gz'
+            new_name_1 = '%s_%s_1.fastq.gz' % (self.sample, self.get_filename_base())
+            new_name_2 = '%s_%s_2.fastq.gz' % (self.sample, self.get_filename_base())
             file_renames.append((old_name_1, new_name_1))
             file_renames.append((old_name_2, new_name_2))
         else:
             old_name = self.accession + '.fastq.gz'
-            new_name = (self.get_filename_base() + '_' + self.experiment.get_platform_short() +
-                        '.fastq.gz')
+            new_name = '%s_%s_%s.fastq.gz' % (self.sample, self.accession, self.experiment.get_platform_short())
             file_renames.append((old_name, new_name))
 
         for old_name, new_name in file_renames:
