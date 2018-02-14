20bd6a97ed243b5751b19d1d194b7e7f749875d8
Ryan Wick <rrwick@gmail.com>
2018-02-14 14:51:20 +1100

    Add README, license and setup.py

--- download_reads.py
+++ download_reads.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 """
-Read downloading script!
+Copyright 2018 Jane Hawkey (jane.hawkey@unimelb.edu.au)
+https://github.com/jhawkey/sra_read_downloader
+
+SRA Read Downloader
+
+This file is part of SRA Read Downloader. SRA Read Downloader is free software: you can
+redistribute it and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+version. SRA Read Downloader is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the GNU General Public License for more details. You should have received a copy of
+the GNU General Public License along with SRA Read Downloader. If not, see
+<http://www.gnu.org/licenses/>.
 
 TO DO:
 * catch "ftplib.error_perm: 550 pathogen/Results/Eschericia_coli_Shigella:
